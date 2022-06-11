565bec116b945db1d63f2e7230df1932ff08d4d4
gabe19p <g19purselley@gmail.com>
2022-06-11 12:22:28 +0900

    week-3 push (unfinished)

---
+++ week-3/js/bill.js
@@ -0,0 +1,45 @@
+import { Product } from "./product.js";
+
+export class Bill {
+  constructor() {}
+  _beverages = [];
+  _appetizers = [];
+  _mainCourses = [];
+  _desserts = [];
+
+  addBeverage(beverage) {
+    this._beverages.push(beverage);
+  }
+
+  addAppetizer(appetizer) {
+    this._appetizers.push(appetizer);
+  }
+
+  addMainCourse(mainCourse) {
+    this._mainCourses.push(mainCourse);
+  }
+
+  addDessert(dessert) {
+    this._desserts.push(dessert);
+  }
+
+  getTotal() {
+    let total = 0;
+
+    let beverageTotal = this._beverages.forEach((beverage) => {
+      total += parseFloat(beverage.price);
+    });
+
+    let appetizerTotal = this._appetizers.forEach((appetizer) => {
+      total += parseFloat(appetizer.price);
+    });
+
+    let mainCourseTotal = this._mainCourses.forEach((mainCourse) => {
+      total += parseFloat(mainCourse.price);
+    });
+
+    let dessertTotal = this._desserts.forEach((dessert) => {
+      total += parseFloat(dessert.price);
+    });
+  }
+}
